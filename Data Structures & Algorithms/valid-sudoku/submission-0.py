f6534eeeaf74b68cb826009fb26c0a04457d2ddf
Heisenburg <kevinanderson1410@gmail.com>
2026-09-02 11:13:01 +0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,49 @@
+import copy
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = len(board)
+        rows = len(board[0])
+        rowLookup = {}
+        colLookup = {}
+        subBoxes = {}
+
+        keys=[]
+        for i in range(0, 9):
+            rowLookup[i] = {}
+            colLookup[i] = {}
+            keys.append(i)
+            if len(keys) == 3:
+                subBoxes.update(dict.fromkeys(keys, {}))
+                keys = []
+
+        subBox = copy.deepcopy(subBoxes)
+        for i in range(cols):
+            if i == 3 or i == 6:
+                subBox = copy.deepcopy(subBoxes)
+
+            for j in range(rows):
+                if board[i][j] == ".":
+                    continue
+                
+                if board[i][j] not in rowLookup[i]:
+                    rowLookup[i][board[i][j]] = True
+                else:
+                    print("Here row", i, j)
+                    return False
+
+                if board[i][j] not in colLookup[j]:
+                    colLookup[j][board[i][j]] = True
+                else:
+                    print("Here col", i, j)
+                    return False
+
+                if board[i][j] not in subBox[j]:
+                    subBox[j][board[i][j]]=True
+                else:
+                    print("Here subBoxes", i, j)
+                    print(subBox[j])
+                    return False
+
+        return True    
+
